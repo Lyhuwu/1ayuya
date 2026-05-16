df58c5fbce89e26554b532dea30ec70d35dc76e1
Lyhuwu <jeydon45@gmail.com>
2026-05-15 18:49:31 -0600

    Update script.js

--- script.js
+++ script.js
@@ -3,20 +3,24 @@ const btnCuriosidades = document.getElementById("btn-curiosidades");
 const spanCerrar = document.getElementsByClassName("cerrar-modal")[0];
 const secretos = document.querySelectorAll('.secreto-item');
 
+// Abrir la ventana de secretos
 btnCuriosidades.onclick = function() {
     modal.style.display = "flex";
 }
 
+// Cerrar con la X
 spanCerrar.onclick = function() {
     modal.style.display = "none";
 }
 
+// Cerrar si se toca fuera del cuadro
 window.onclick = function(event) {
     if (event.target == modal) {
         modal.style.display = "none";
     }
 }
 
+// Efecto acordeón para abrir cada secreto individual
 secretos.forEach(secreto => {
     secreto.addEventListener('click', () => {
         const respuesta = secreto.querySelector('.secreto-respuesta');
